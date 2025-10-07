4130204802306ee535f579b6d28cdb029b75aa0e
Mikołaj Warda <mikolaj.warda@gmail.com>
2025-10-07 15:20:35 +0200

    feat: Basic streamlit main page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+import streamlit as st
+    
 def main():
-    print("Hello from app!")
+    st.title("Loan Assistant", width="content")
+    st.write("Welcome to the Loan Assistant! This application helps you determine your eligibility for a loan based on various financial and personal factors. Please provide the necessary information, and we'll guide you through the process.")
+    st.button("Get Started!", icon="🚀")
 
 
 if __name__ == "__main__":
