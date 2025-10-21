3fb2d222dbcc72b214c6562c772149df6be19f66
Mikołaj Warda <mikolaj.warda@gmail.com>
2025-10-21 21:53:00 +0200

    feat: Implement code changes to enhance functionality of neural network module

--- neural_core/models/neural_network.py
+++ neural_core/models/neural_network.py
@@ -1,8 +1,13 @@
 import torch.nn as nn
 from torch.nn import functional as F
 from torch import softmax
+from torch.utils.data import DataLoader, Dataset
 from typing import override
-
+import pandas as pd
+import torch
+from torch.optim import Adam
+from torch.nn import BCEWithLogitsLoss
+from sklearn.model_selection import train_test_split
 
 class BaseLoanNN(nn.Module):
     """ A base neural network class for loan prediction tasks.
@@ -20,6 +25,37 @@ class BaseLoanNN(nn.Module):
         x = self.output_layer(x)
         return x
     
+    
+    def perform_training(self, train_dataset:Dataset, optimizer, criterion ,epochs:int=50, batch_size:int=32, enable_validation:bool = False) -> None:
+        data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        if enable_validation:
+            train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.2)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        
+        for epoch in range(epochs):
+            self.train()
+            epoch_loss = 0.0
+            for X, y in data_loader:
+                optimizer.zero_grad()
+                outputs = self(X)
+                loss = criterion(outputs, y.reshape(-1, 1))
+                loss.backward()
+                optimizer.step()
+                epoch_loss += loss.item()
+            
+            print(f'Epoch [{epoch+1}/{epochs}], BCE: {epoch_loss/len(data_loader):.4f}')
+            
+            if enable_validation:
+                self.eval()
+                val_loss = 0.0
+                with torch.no_grad():
+                    for X_val, y_val in val_loader:
+                        val_outputs = self(X_val)
+                        v_loss = criterion(val_outputs, y_val.reshape(-1, 1))
+                        val_loss += v_loss.item()
+                print(f'Validation BCE: {val_loss/len(val_loader):.4f}')
+        
+        
     
     @override
     def __str__(self):
