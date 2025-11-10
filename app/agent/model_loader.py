2656a823e20cbd7a5d96236cfe785a2b690b7ad9
Mikołaj Warda <mikolaj.warda@gmail.com>
2025-11-10 14:30:51 +0100

    feat: Added agent graph

--- app/agent/model_loader.py
+++ app/agent/model_loader.py
@@ -12,24 +12,20 @@ import joblib
 
 
 class ModelLoader:
-    def __init__(self):
-        self.model = self._load_model()
-        self.scaler = self._load_scaler()
-        self.categorical_encoders = self._load_categorical_encoders()
-
-    def _load_model(self):
+    
+    @staticmethod
+    def _load_model():
         model = BaseLoanNN(input_size=13, hidden_layers=nn.ModuleList([nn.Linear(13, 64)]), output_size=1)
         model.load_state_dict(torch.load(Path(__file__).parent / 'modules' / 'model.pth'))
         return model
-
-    def _load_scaler(self):
-        return joblib.load(Path(__file__).parent / 'modules' / 'numeric_scaler.joblib')
-
-    def _load_categorical_encoders(self):
-        return joblib.load(Path(__file__).parent / 'modules' / 'categorical_encoders.joblib')
     
-    def __str__(self):
-        return f"Model: {self.model}\nScaler: {self.scaler}\nCategorical Encoders: {self.categorical_encoders}"
+    @staticmethod
+    def _load_scaler():
+        return joblib.load(Path(__file__).parent / 'modules' / 'numeric_scaler.joblib')
+    
+    @staticmethod
+    def _load_categorical_encoders():
+        return joblib.load(Path(__file__).parent / 'modules' / 'categorical_encoders.joblib')
     
     
 if __name__ == "__main__":
