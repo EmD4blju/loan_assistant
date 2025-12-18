9f6d71fe1837900f2b36070072b863eefb806f35
Mikołaj Warda <mikolaj.warda@gmail.com>
2025-12-18 14:46:38 +0100

    fix: Adjusted code for deployment

--- app/agent/model_loader.py
+++ app/agent/model_loader.py
@@ -6,21 +6,22 @@ import torch.nn as nn
 from .neural_network import BaseLoanNN
 import joblib
 
+# models
 
 class ModelLoader:
 
     @staticmethod
     def _load_model():
-        model = torch.load(Path(__file__).parents[2] / 'models' / 'temp_scaled_loan_model.pth', weights_only=False)
+        model = torch.load(Path('models', 'temp_scaled_loan_model.pth'), weights_only=False)
         return model
 
     @staticmethod
     def _load_scaler():
-        return joblib.load(Path(__file__).parents[2] / 'models' / 'feature_scaler.joblib')
+        return joblib.load(Path('models', 'feature_scaler.joblib'))
 
     @staticmethod
     def _load_categorical_encoders():
-        return joblib.load(Path(__file__).parents[2] / 'models' / 'feature_encoders.joblib')
+        return joblib.load(Path('models', 'feature_encoders.joblib'))
 
 
 if __name__ == "__main__":
